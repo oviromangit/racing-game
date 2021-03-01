b42c56a71ca2b5b9bfce2ba339ba2827edf65ba5
Ovidiu Roman <oviroman77@gmail.com>
2021-03-01 09:59:16 +0200

    deleted comments

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,9 +1,5 @@
 package org.fasttrackit;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
@@ -11,3 +7,4 @@ public class App
         System.out.println( "Hello World!" );
     }
 }
+
